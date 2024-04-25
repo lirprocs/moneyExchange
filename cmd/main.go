b61bcf59b20197ac0c440ed51ce79c1bf8034d51
Lirprocs <lirprocs@gmail.com>
2024-04-26 02:36:33 +0300

    v1.0

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,89 @@
 package main
 
 import (
-	"moneyExchange/internal/config"
+	"context"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log/slog"
+	"moneyExchangeTest/internal/config"
+	"moneyExchangeTest/internal/http-server/handlers/exchange"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	//fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("Starting server")
 
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/exchange", exchange.New(log))
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	server := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("failed to start server")
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+	log.Info("shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server: %s\n", err)
+	}
+	log.Info("server stopped gracefully.")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
---
+++ internal/http-server/handlers/exchange/exchange.go
@@ -0,0 +1,62 @@
+package exchange
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+)
+
+type Request struct {
+	Amount    int   `json:"amount"`
+	Banknotes []int `json:"banknotes"`
+}
+
+type Response struct {
+	Exchanges [][]int `json:"exchanges"`
+}
+
+func New(log *slog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.exchange.New"
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", err)
+
+			render.JSON(w, r, fmt.Errorf("failed to decode request"))
+
+			return
+		}
+		log.Info("request body decoded", slog.Any("request", req))
+
+		var exchange [][]int
+		calculateExchange(req.Amount, req.Banknotes, []int{}, &exchange)
+
+		log.Info("The count has been completed")
+		render.JSON(w, r, Response{Exchanges: exchange})
+		log.Info("", exchange)
+	}
+}
+
+func calculateExchange(amount int, banknotes []int, currentExchange []int, result *[][]int) {
+	if amount == 0 {
+		temp := make([]int, len(currentExchange))
+		copy(temp, currentExchange)
+		*result = append(*result, temp)
+		return
+	}
+
+	for i, note := range banknotes {
+		if note <= amount {
+			calculateExchange(amount-note, banknotes[i:], append(currentExchange, note), result)
+		}
+	}
+}
